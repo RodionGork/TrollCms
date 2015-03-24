19f0895fbc4144ec59e34901bc96f8e521c67dbe
Rodion Gorkovenko <rodiongork@github.com>
2015-03-24 22:19:03 +0300

    Aside fragment and new page button

--- pages/editlist.php
+++ pages/editlist.php
@@ -5,3 +5,19 @@
     </li>
 <?php endforeach; ?>
 </ul>
+
+<div>
+    New file:
+    <input placeholder="Name" id="new-name"/>
+    <input type="button" id="new-button" value="Create"/>
+</div>
+
+<script>
+$(function() {
+    $('#new-button').click(function() {
+        location.href = location.href + '/' + $('#new-name').val();
+    });
+});
+
+</script>
+
